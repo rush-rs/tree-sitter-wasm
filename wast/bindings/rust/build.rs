70cdabfe558400d40354f13d33bcf2cce6a4a4bb
RubixDev <silas.groh@t-online.de>
2023-03-29 11:07:18 +0200

    feat: add rust bindings with c2rust support

---
+++ wast/bindings/rust/build.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let src_dir = std::path::Path::new("src");
+
+    #[cfg(not(feature = "tree-sitter-c2rust"))]
+    {
+        let mut c_config = cc::Build::new();
+        c_config.include(src_dir);
+        c_config
+            .flag_if_supported("-Wno-unused-parameter")
+            .flag_if_supported("-Wno-unused-but-set-variable")
+            .flag_if_supported("-Wno-trigraphs");
+        let parser_path = src_dir.join("parser.c");
+        c_config.file(&parser_path);
+
+        c_config.compile("parser");
+        println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
+    }
+
+    #[cfg(feature = "tree-sitter-c2rust")]
+    {
+        tree_sitter_wasm_build_tool::compile_parser(
+            &src_dir.join("parser.c"),
+            &src_dir.join("tree_sitter/parser.h"),
+        )
+        .unwrap();
+    }
+}
